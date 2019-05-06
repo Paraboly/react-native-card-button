391f8adfa34e5c692c5edf23d5dea68b5df74989
FreakyCoder <kurayogun@gmail.com>
2019-05-06 14:12:17 +0300

    Example and Lib is integrated. Time to test

---
+++ example/App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CardButton from "./lib/src/components/CardButton";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <CardButton />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
---
+++ example/lib/src/components/CardButton.js
@@ -0,0 +1,150 @@
+import React from "react";
+import PropTypes from "prop-types";
+import colors from "../../styles/colors";
+import { Text, View } from "react-native";
+import Ripple from "react-native-material-ripple";
+import Icon from "react-native-dynamic-vector-icons";
+import LinearGradient from "react-native-linear-gradient";
+import styles, {
+  container,
+  textStyle,
+  normalTypeContainer
+} from "./styles/CardButton.style";
+
+const CardButton = props => {
+  function renderContainer() {
+    const {
+      text,
+      iconName,
+      iconSize,
+      iconType,
+      iconColor,
+      textSize,
+      textColor,
+      iconComponent,
+      textComponent,
+      textFontFamily
+    } = props;
+    return (
+      <View style={styles.column}>
+        {iconComponent || (
+          <View>
+            <Icon
+              size={iconSize}
+              name={iconName}
+              type={iconType}
+              color={iconColor}
+            />
+          </View>
+        )}
+        {textComponent || (
+          <Text style={textStyle(textSize, textColor, textFontFamily)}>
+            {text}
+          </Text>
+        )}
+      </View>
+    );
+  }
+
+  function renderGradient() {
+    const {
+      end,
+      start,
+      width,
+      height,
+      onPress,
+      rippleColor,
+      borderRadius,
+      gradientColors,
+      rippleContainerBorderRadius
+    } = props;
+    return (
+      <Ripple
+        onPress={onPress}
+        rippleColor={rippleColor}
+        rippleContainerBorderRadius={rippleContainerBorderRadius}
+      >
+        <LinearGradient
+          end={end}
+          start={start}
+          colors={gradientColors}
+          style={[
+            container(height, width, borderRadius),
+            shadowStyle || styles.shadowStyle
+          ]}
+        >
+          {renderContainer()}
+        </LinearGradient>
+      </Ripple>
+    );
+  }
+
+  function renderNormal() {
+    const {
+      width,
+      height,
+      onPress,
+      rippleColor,
+      shadowStyle,
+      borderRadius,
+      backgroundColor,
+      rippleContainerBorderRadius
+    } = props;
+    return (
+      <Ripple
+        onPress={onPress}
+        rippleColor={rippleColor}
+        rippleContainerBorderRadius={rippleContainerBorderRadius}
+      >
+        <View
+          style={[
+            normalTypeContainer(height, width, borderRadius, backgroundColor),
+            shadowStyle || styles.shadowStyle
+          ]}
+        >
+          {renderContainer()}
+        </View>
+      </Ripple>
+    );
+  }
+
+  const { gradient } = props;
+  const renderCardButton = gradient ? renderGradient() : renderNormal();
+  return renderCardButton;
+};
+
+CardButton.propTypes = {
+  end: PropTypes.object,
+  text: PropTypes.string,
+  start: PropTypes.object,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  iconName: PropTypes.string,
+  iconSize: PropTypes.number,
+  iconType: PropTypes.string,
+  iconColor: PropTypes.string,
+  rippleColor: PropTypes.string,
+  borderRadius: PropTypes.number,
+  gradientColors: PropTypes.array,
+  backgroundColor: PropTypes.string,
+  rippleContainerBorderRadius: PropTypes.number
+};
+
+CardButton.defaultProps = {
+  text: "",
+  width: 90,
+  height: 90,
+  iconSize: 16,
+  borderRadius: 20,
+  iconName: "traffic",
+  end: { x: 0, y: 1 },
+  start: { x: 0.5, y: 0 },
+  iconType: "MaterialIcons",
+  backgroundColor: "#fcfcfc",
+  rippleContainerBorderRadius: 20,
+  rippleColor: colors.theme.light.primary,
+  iconColor: colors.theme.light.lightenGrey,
+  gradientColors: ["#f9f7b4", "#f7c77e", "#e8ba6b"]
+};
+
+export default CardButton;
